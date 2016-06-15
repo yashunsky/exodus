72201ff5100b087705f5a71606accf020034f913
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2016-06-15 23:37:32 +0300

    initial commit

---
+++ torah/torah_generator.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python2.7
+
+from os import walk
+from os.path import join
+
+from PIL import Image  # pip install Pillow
+
+PARTS_PATH = 'parts'
+TORAH_HEMPLATE_PATH = 'torah_template.html'
+OUTPUT = 'index.html'
+
+PAGE_HEIGHT = 800
+PAGE_WIDTH = 210 * 800 / 297
+
+ANCHOR_HEIGHT = 10
+
+MAIN_TEMPLATE = '<td colspan="{colspan}"><img src="{filename}" width="{width}" height="{height}"></td>'
+ANCHOR_TEMPLATE = '<td align="center"><a name="{page}">&nbsp;</a></td>'
+CONTENT_TEMPLATE = ' <a href="#{page}">{page}</a> '
+
+
+def main_row_segment(filename, width, height, pages_count):
+    return MAIN_TEMPLATE.format(colspan=pages_count,
+                                filename=filename,
+                                width=width,
+                                height=height)
+
+
+def anchor_row_segment(pages_count, start_number):
+    return ''.join([ANCHOR_TEMPLATE.format(page=page) for page
+                    in xrange(start_number, start_number + pages_count)])
+
+if __name__ == '__main__':
+    _, _, filenames = walk(PARTS_PATH).next()
+    filenames = [join(PARTS_PATH, f) for f in sorted(filenames)]
+
+    page_number = 1
+    main_row = ''
+    anchor_row = ''
+    content_table = ''
+
+    for filename in filenames:
+        img = Image.open(filename)
+        width, height = img.size
+        assert height == PAGE_HEIGHT
+
+        pages_count = width / PAGE_WIDTH
+        main_row += main_row_segment(filename, width, height, pages_count)
+        anchor_row += anchor_row_segment(pages_count, page_number)
+        page_number += pages_count
+
+    content_table = ''.join([CONTENT_TEMPLATE.format(page=page)
+                             for page in xrange(1, page_number + 1)])
+
+    with open(TORAH_HEMPLATE_PATH, 'r') as t:
+        template = t.read()
+        with open(OUTPUT, 'w') as o:
+            o.write(template.format(torah_width=PAGE_WIDTH * 2,
+                                    torah_height=PAGE_HEIGHT + ANCHOR_HEIGHT * 2,
+                                    page_height=PAGE_HEIGHT,
+                                    anchor_height=ANCHOR_HEIGHT,
+                                    main_row=main_row,
+                                    anchor_row=anchor_row,
+                                    content_table=content_table))
